ecf4378d51b97e085a729eb847d05121e85aff2d
Eason Chiang <ishiu.chiang@gmail.com>
2017-05-20 20:22:21 +0800

    load access token by dotenv

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
+require('dotenv-safe').load();
+
 const line = require('@line/bot-sdk');
 const Koa = require('koa');
 const Router = require('koa-router');
-const c2k = require('koa-connect')
+const c2k = require('koa-connect');
 
 const config = {
-  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN,
-  channelSecret: process.env.CHANNEL_SECRET,
+  channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
+  channelSecret: process.env.LINE_CHANNEL_SECRET,
 };
 const client = new line.Client(config);
 

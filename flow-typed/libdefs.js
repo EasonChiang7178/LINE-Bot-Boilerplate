75089157ce8bf4317661fdcd435c1125c4ddf600
Eason Chiang <ishiu.chiang@gmail.com>
2017-05-21 21:28:23 +0800

    add usage info for flow-typed/libdefs.js

--- flow-typed/libdefs.js
+++ flow-typed/libdefs.js
@@ -1,4 +1,17 @@
-declare module 'dotenv-safe' { declare var exports: any; }
-declare module 'koa' { declare var exports: any; }
-declare module 'koa-router' { declare var exports: any; }
-declare module 'koa2-better-body' { declare var exports: any; }
+/*
+ * place and declare exports type for any used node_modules in project,
+ * in order to prevent Flowtype warning.
+ **/
+
+declare module 'dotenv-safe' {
+  declare var exports: any
+}
+declare module 'koa' {
+  declare var exports: any
+}
+declare module 'koa-router' {
+  declare var exports: any
+}
+declare module 'koa2-better-body' {
+  declare var exports: any
+}

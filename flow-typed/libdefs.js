d1e02d092fd694659efb5bb7127bdab03df8fec5
Eason Chiang <ishiu.chiang@gmail.com>
2017-05-21 19:51:45 +0800

    add flow type to workflow

---
+++ flow-typed/libdefs.js
@@ -0,0 +1,4 @@
+declare module 'dotenv-safe' { declare var exports: any; }
+declare module 'koa' { declare var exports: any; }
+declare module 'koa-router' { declare var exports: any; }
+declare module 'koa2-better-body' { declare var exports: any; }

dc53f371d64e820e78a1338b7fb659b0029d4f94
Eason Chiang <ishiu.chiang@gmail.com>
2017-05-21 10:43:29 +0800

    get request event from LINE webhook

--- index.js
+++ index.js
@@ -1,26 +1,22 @@
 require('dotenv-safe').load();
 
-const line = require('@line/bot-sdk');
 const Koa = require('koa');
 const Router = require('koa-router');
-const c2k = require('koa-connect');
+const bodyParser = require('koa2-better-body')
 
 const config = {
   channelAccessToken: process.env.LINE_CHANNEL_ACCESS_TOKEN,
   channelSecret: process.env.LINE_CHANNEL_SECRET,
 };
-const client = new line.Client(config);
 
 const router = new Router();
-router.post(
-  '/line-webhook',
-  c2k(line.middleware(config)),
-  async function (ctx, next) {
-    console.log(ctx.request.events)
-    const replies = await Promise.all(ctx.request.events.map(handleEvent))
-    console.log(replies)
-    ctx.body = replies
-})
+router.post('/line-webhook', bodyParser(), async function (ctx, next) {
+    console.log(ctx.request.fields);
+    await next();
+    // const replies = await Promise.all(ctx.request.events.map(handleEvent))
+    // console.log(replies)
+    // ctx.body = replies
+});
 
 const app = new Koa();
 app.use(router.routes());

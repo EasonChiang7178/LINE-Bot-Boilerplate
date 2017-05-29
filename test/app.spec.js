ca9b7ac04529c7cedfd5eac415953420a5ce2003
Eason Chiang <ishiu.chiang@gmail.com>
2017-05-30 00:00:18 +0800

    hello test

--- test/app.spec.js
+++
@@ -1,8 +0,0 @@
-const request = require('supertest')
-const Koa = require('koa')
-
-const app = new Koa()
-
-it('should return status code 404 for all request', function(done) {
-  request(app.callback()).post('/line-webhook').expect(404, done)
-})
